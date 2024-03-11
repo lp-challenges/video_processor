30096457e3b3a92f023c4af7c391846069ba54f1
Lucas <lucaspedrazoli@gmail.com>
2024-03-11 17:21:54 -0300

    adds implementation

--- IrisCamera/Sources/IrisCamera/Internal/SampleBufferTransformer.swift
+++ IrisCamera/Sources/IrisCamera/Internal/SampleBufferTransformer.swift
@@ -10,8 +10,22 @@ struct SampleBufferTransformer {
     }
 
     func transform(videoSampleBuffer: CMSampleBuffer, with filter: Filter) -> CMSampleBuffer {
-        
-        return videoSampleBuffer
+        let context = CIContext(options: nil)
+        guard let cvImageBuffer = CMSampleBufferGetImageBuffer(videoSampleBuffer) else {
+            print("failed to get pixel buffer")
+            fatalError()
+        }
+        let ciMask: CIImage = CIImage(cvImageBuffer: cvImageBuffer)
+        let filter = CIFilter(name: filter.rawValue, parameters: [kCIInputImageKey: ciMask])
+
+        guard let outputImage: CIImage = filter?.outputImage else { fatalError() }
+        context.render(outputImage, to: cvImageBuffer)
+
+        let presentationTimestamp = CMSampleBufferGetPresentationTimeStamp(videoSampleBuffer)
+        guard let result = try? cvImageBuffer.mapToSampleBuffer(timestamp: presentationTimestamp) else {
+            fatalError()
+        }
+        return result
     }
 }
 
